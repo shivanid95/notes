89f80eb95cca0a6e5058cfcb7007c3abb862145c
Shivani <shivai.dosajh@smallcase.com>
2020-02-16 22:44:41 +0530

    create note fragment added
    Added layout and fragment for create note view, and linked navigation

---
+++ app/src/main/java/com/example/notes/createNote/CreateNoteFragment.kt
@@ -0,0 +1,23 @@
+package com.example.notes.createNote
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.notes.R
+import com.example.notes.databinding.CreateNoteFragmentBinding
+
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<CreateNoteFragmentBinding>(inflater, R.layout.create_note_fragment, container, false)
+        return binding.root
+
+    }
+}
