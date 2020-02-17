0ff2d0a509576ed22cf9ad32546e570782745cec
Shivani <shivai.dosajh@smallcase.com>
2020-02-17 13:37:58 +0530

    Create Note back navigation
    Added back press handling for create note fragment and UI refactor

--- app/src/main/java/com/example/notes/createNote/CreateNoteViewModel.kt
+++ app/src/main/java/com/example/notes/createNote/CreateNoteViewModel.kt
@@ -9,7 +9,14 @@ import com.example.notes.data.Note
 import com.example.notes.data.NotesRepository
 import com.example.notes.utility.Event
 
+/**
+ * CreateNoteViewModel handles business logic for note creation.
+ */
 class CreateNoteViewModel: ViewModel() {
+
+    /**
+     * Data Bindings
+     */
 
     // Two-way databinding, exposing MutableLiveData
     val title = MutableLiveData<String>()
@@ -17,21 +24,27 @@ class CreateNoteViewModel: ViewModel() {
     // Two-way databinding, exposing MutableLiveData
     val content = MutableLiveData<String>()
 
+    // Live on Click event emitter for saving notes
     private val _saveNoteEvent = MutableLiveData<Event<Unit>>()
     val saveNoteEvent: LiveData<Event<Unit>>
         get() = _saveNoteEvent
 
-
+    // Show Snackbar event emitter (when save cannot be performed)
     private val _snackbarText = MutableLiveData<Event<Int>>()
     val snackbarMessage: LiveData<Event<Int>>
         get() = _snackbarText
 
+    /**
+     * Utitly Variables
+     */
 
+    // Returns true if content and title are not empty
     val canSaveNote : Boolean
         get() {
             return isTitleValid && isContentValid
         }
 
+    // Returns true if title is not emoty and number of characters are less than 100
     val isTitleValid: Boolean
         get() {
             val titleStr = title.value
@@ -45,17 +58,20 @@ class CreateNoteViewModel: ViewModel() {
         }
 
 
-
+    // Adds note to the notes Repository
     fun saveNote() {
         Log.d("Content", "${title.value} content: ${content.value}")
         if (canSaveNote) {
             val note = Note(title.value!!, content.value!!)
-            _saveNoteEvent.value = Event(Unit)
             NotesRepository.add(note)
+            // Emits snackbar event
+            _saveNoteEvent.value = Event(Unit)
+
         }
         else if (!isTitleValid) {
-                _snackbarText.value = Event(R.string.no_title_message)
-            }
+            // Emits snackbar event
+            _snackbarText.value = Event(R.string.no_title_message)
+        }
 
         else if (!isContentValid) {
             _snackbarText.value = Event(R.string.no_content_message)
--- app/src/main/java/com/example/notes/noteDetails/NoteDetailsFragment.kt
+++ app/src/main/java/com/example/notes/noteDetails/NoteDetailsFragment.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.notes.R
+import com.example.notes.createNote.CreateNoteFragmentDirections
 import com.example.notes.data.Note
 import com.example.notes.databinding.NoteDetailsFragmentBinding
 
 class NoteDetailsFragment: Fragment() {
 
     lateinit var note: Note
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // Handle back press
+       requireActivity().onBackPressedDispatcher.addCallback(this) {
+          findNavController().navigate(NoteDetailsFragmentDirections.actionNoteDetailsFragmentToNotesListFragment(null, ""))
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +38,7 @@ class NoteDetailsFragment: Fragment() {
             print(noteObject)
             note = noteObject
         }
+        activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
         binding.note = note
         return binding.root
     }
